7c88a6eee8ca9917399b39377d65ab6eea4e790d
Laurence <laurencepeacock@gmail.com>
2022-01-27 10:37:21 +0000

    Update blogToggle.js

--- JS/blogToggle.js
+++ JS/blogToggle.js
@@ -9,3 +9,24 @@ function readMore (){
 
 button1.addEventListener('click', readMore);
 
+let readMore2 = document.getElementById('read-more-2');
+let button2 = document.getElementById('read-more-button-2');
+
+function readMore (){
+    readMore2.classList.toggle('read-more');
+  
+    
+}
+
+button2.addEventListener('click', readMore);
+
+let readMore3 = document.getElementById('read-more-3');
+let button3 = document.getElementById('read-more-button-3');
+
+function readMore (){
+    readMore3.classList.toggle('read-more');
+  
+    
+}
+
+button3.addEventListener('click', readMore);
