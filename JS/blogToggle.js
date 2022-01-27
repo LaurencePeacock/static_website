7fa65d655ef0ff8bd2edd3a5e6d67e1b34b47361
Laurence <laurencepeacock@gmail.com>
2022-01-27 10:29:47 +0000

    Update blogToggle.js

--- JS/blogToggle.js
+++ JS/blogToggle.js
@@ -1,5 +1,5 @@
-let readMore1 = document.getElementsById('read-more-1');
-let button1 = document.getElementsById('read-more-button-1');
+let readMore1 = document.getElementById('read-more-1');
+let button1 = document.getElementById('read-more-button-1');
 
 function readMore (){
     readMore1.classList.toggle('read-more');
